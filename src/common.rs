497786123021482b67cb9381d92cfddcf6648ae7
t11s <greenfeatherhelp@gmail.com>
2025-07-06 23:36:45 -0700

    feat: 1m

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-pub const DATA_SIZE: usize = 100_000;
+pub const DATA_SIZE: usize = 1_000_000;
 
 pub fn generate_test_data() -> Vec<(u64, u64)> {
     let mut rng = rand::thread_rng();
