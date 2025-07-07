7accc22e353b8f6f0676947f0ff39fc22f853eb2
t11s <greenfeatherhelp@gmail.com>
2025-07-06 23:59:56 -0700

    nits: datasize / ordering

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use crate::common::DATA_SIZE;
 
 fn main() {
     println!("Benchmarking {DATA_SIZE} batch writes and reads of random u64 key-value pairs\n");
-    sled::benchmark_sled();
-    redb::benchmark_redb();
-    hashmap::benchmark_hashmap();
     hashmap::benchmark_hashmap_noncrypto();
+    hashmap::benchmark_hashmap();
     libmdbx::benchmark_libmdbx();
+    redb::benchmark_redb();
+    sled::benchmark_sled();
 }
